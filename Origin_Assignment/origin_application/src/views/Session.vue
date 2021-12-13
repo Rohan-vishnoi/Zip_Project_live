a7c7e8d9b814efe7ed88e07494e82a043a2b82a1
Rohan Vishnoi <avirohanvishnoi@gmail.com>
2021-12-13 13:37:56 +0530

    final deoplyment file

--- Origin_Assignment/origin_application/src/views/Session.vue
+++ Origin_Assignment/origin_application/src/views/Session.vue
@@ -2,29 +2,70 @@
   <div class="Session">
     <h2 style="float: left; margin-left: 55px">
       <b>FILTERS</b>
+      <b-list-group-item @click="Defaultcontent()" style="width: fit-content"
+        >All</b-list-group-item
+      >
       <b-list-group
         class="labelfilter"
         v-for="label in labelarray"
         :key="label"
       >
-        <b-list-group-item style="width: fit-content">{{
-          label
-        }}</b-list-group-item>
+        <b-list-group-item
+          @click="selectobjects(label)"
+          style="width: fit-content"
+          >{{ label }}</b-list-group-item
+        >
       </b-list-group>
     </h2>
-    <div class="containerimages">
-      <a class="images" v-for="images in Imagesarray" :key="images">
-        <img :src="images" style="width: 200px; height: 200px; margin: 10px" />
+    <div class="containerimages" v-show="searchbar == false">
+      <a class="images" v-for="images in Imagesarray" :key="images.label">
+        <img
+          :src="images.image"
+          style="width: 200px; height: 200px; margin: 10px"
+        />
+
         <b-form inline>
-          <label class="sr-only" for="inline-form-input-name">Name</label>
+          <h3 class="headingthumbnails">{{ images.label }}</h3>
           <b-form-input
+            v-if="images.label == ''"
             id="inline-form-input-name"
             class="mb-2 mr-sm-2 mb-sm-0"
             placeholder="ENTER  LABEL"
+            v-model="textlabel"
           ></b-form-input>
-          <b-button variant="info">Add</b-button>
+          <b-button
+            v-if="images.label == ''"
+            @click="AddLabel(images)"
+            variant="info"
+            >Add</b-button
+          >
         </b-form>
       </a>
+    </div>
+    <div class="containerimages" v-show="searchbar == true">
+       <a class="images" v-for="images in searchedarray" :key="images.label">
+        <img
+          :src="images.image"
+          style="width: 200px; height: 200px; margin: 10px"
+        />
+
+        <b-form inline>
+          <h3 class="headingthumbnails">{{ images.label }}</h3>
+          <b-form-input
+            v-if="images.label == ''"
+            id="inline-form-input-name"
+            class="mb-2 mr-sm-2 mb-sm-0"
+            placeholder="ENTER  LABEL"
+            v-model="textlabel"
+          ></b-form-input>
+          <b-button
+            v-if="images.label == ''"
+            @click="AddLabel(images)"
+            variant="info"
+            >Add</b-button
+          >
+        </b-form>
+      </a> 
     </div>
   </div>
 </template>
@@ -34,14 +75,55 @@ export default {
   components: {},
   data() {
     return {
+      textlabel: "",
       labelarray: [],
       Imagesarray: [],
+      hash: new Map(),
+      selectedobjects: "",
+      searchbar: false,
+      searchedarray: [],
     };
   },
-  methods: {},
+  methods: {
+    AddLabel(image) {
+      this.Imagesarray.forEach((element) => {
+        if (image.image === element.image && image.label == "") {
+          element.label = this.textlabel;
+        }
+      });
+      this.textlabel = "";
+    },
+    selectobjects(name) {
+      this.selectedobjects = name;
+      this.searchbar = true;
+
+      this.Imagesarray.forEach((element) => {
+        if (element.label.toLowerCase() == this.selectedobjects.toLowerCase()) {
+          var newobj = {};
+          newobj.image = element.image;
+          newobj.label = element.label;
+          this.searchedarray.push(newobj);
+          newobj = {};
+        }
+      });
+    },
+    Defaultcontent() {
+      this.searchbar = false;
+      this.searchedarray = [];
+    },
+  },
   mounted() {
     this.labelarray = this.$store.getters.getlabelset;
-    this.Imagesarray = this.$store.getters.getuploadfile;
+    var newobj = {};
+    //this.Imagesarray = this.$store.getters.getuploadfile;
+
+    this.$store.getters.getuploadfile.forEach((element) => {
+      newobj.image = element;
+      newobj.label = "";
+      this.Imagesarray.push(newobj);
+      newobj = {};
+    });
+    console.log(this.Imagesarray);
   },
 };
 </script>
@@ -66,11 +148,15 @@ img:hover {
   margin-left: 48px;
 }
 .containerimages {
-  display: inline-flex;
+  display: grid;
   align-items: center;
   justify-content: center;
 }
 input#inline-form-input-name {
-    width: 62%;
+  width: 62%;
+}
+h3.headingthumbnails {
+  font-size: 24px;
+  color: snow;
 }
 </style>
